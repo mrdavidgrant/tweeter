92ba17778fb91cb4aa3a1a075a3983a04ff53c28
David Grant <mrdavidgrant@outlook.com>
2017-09-20 15:42:46 -0400

    Accessing MongoDB and posting to MongoDB

--- server/index.js
+++ server/index.js
@@ -6,12 +6,14 @@ const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
 const app           = express();
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // The in-memory database of tweets. It's a basic object with an array in it.
-const db = require("./lib/in-memory-db");
+MongoClient.connect(MONGODB_URI, (err, db) => {
 
 // The `data-helpers` module provides an interface to the database of tweets.
 // This simple interface layer has a big benefit: we could switch out the
@@ -28,6 +30,7 @@ const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
 // Mount the tweets routes at the "/tweets" path prefix:
 app.use("/tweets", tweetsRoutes);
+})
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,18 +9,23 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+        db.collection('tweets').insertOne(newTweet, (err, result) => {
+          if (err) {
+            return callback(err)
+          }
+          
+          callback(null, true)
+        })
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
       simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+        const sortNewestFirst = (a, b) => b.created_at - a.created_at;
+        db.collection('tweets').find().toArray((err, tweets) => {
+          callback(null, tweets)
+        })
+      })
     }
 
   };
