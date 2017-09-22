a8a78c33c228ad0a12a83f6c456ba31eb6223d6b
David Grant <mrdavidgrant@outlook.com>
2017-09-22 11:37:56 -0400

    SASS middleware running

--- server/index.js
+++ server/index.js
@@ -4,10 +4,21 @@ const PORT          = process.env.PORT
 const express       = require("express")
 const bodyParser    = require("body-parser")
 const app           = express()
-const MongoClient = require("mongodb").MongoClient
-const MONGODB_URI = process.env.MONGODB_URI
+const MongoClient   = require("mongodb").MongoClient
+const MONGODB_URI   = process.env.MONGODB_URI
+const path          = require('path')
+const sassMiddleware = require('node-sass-middleware')
+var winston = require('winston')
 
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(sassMiddleware({
+  /* Options */
+  src: path.join(__dirname, '/styles')
+  , response: false
+  , dest: path.join(__dirname, '../public/styles')
+  , outputStyle: 'extended'
+  , prefix: '/styles'
+}));
 app.use(express.static("public"))
 
 MongoClient.connect(MONGODB_URI, (err, db) => {
